f3c9b23e6f670a98e93925b3d32b3fab83db6503
rayz <rayquaza4010@icloud.com>
2025-08-18 15:16:37 +0530

    this is exp_2

---
+++ 5th sem/Computer Networks/bellman_ford.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class bellman_ford {
+    public static void main(String args[]){
+        Scanner scanner= new Scanner(System.in);
+        int num_ver = 0;
+        System.out.println("Enter the number of vertices");
+        num_ver=scanner
+
+    }
+}
